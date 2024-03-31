b9848ab5fb927b26a9c92de50abb783fe28cc351
mi-skam <40042054+mi-skam@users.noreply.github.com>
2024-03-31 17:01:24 +0200

    adding ticker example

--- app.py
+++ app.py
@@ -1,48 +1,16 @@
-# streamlit, a low-code framework used for the front end to let users interact with the app.
-# langchain, a framework for working with LLM models.
+import yfinance as yf
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
-from langchain_openai import OpenAI
+import pandas as pd
 
-from dotenv import load_dotenv
-import os
+st.write("""
+# Simple Stock Price App
 
-load_dotenv()
+Shown are the stock closing price and volume of Google.
+""")
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
+tickerSymbol = "GOOGL"
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(period="1d", start="2014-3-31", end="2024-3-31")
 
-# browser window title
-st.set_page_config(page_title="Zaitz: blog outline generator")
-# app title
-st.title("Zaitz: blog outline generator 🎨🤖")
-
-
-# gets a user's input, sends it to the llm and prints the response in a info box
-def generate_response(input_text):
-    llm = OpenAI(
-        model_name="gpt-3.5-turbo-instruct",
-        temperature=0.7,
-        openai_api_key=openai_api_key,
-    )
-    topic = input_text
-    template = "As an experienced data scientist and technical writer, generate an outline for a blog about {topic}."
-    prompt = PromptTemplate(input_variables=["topic"], template=template)
-    prompt_query = prompt.format(topic=topic)
-    # Run LLM Model
-    response = llm(prompt_query)
-
-    return st.info(response)
-
-
-# this context creates a form
-with st.form("my_form"):
-    topic = st.text_input("Enter keyword:", "")
-    # creates a handler for the submit button, returns True if the button was clicked
-    submitted = st.form_submit_button("Submit")
-
-    # check for wrong key format
-    if not openai_api_key.startswith("sk-"):
-        st.warning("Please enter your OpenAI API key!", icon="⚠")
-    # runs generate_response with the input as arguments
-    if submitted and openai_api_key.startswith("sk-"):
-        generate_response(topic)
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
